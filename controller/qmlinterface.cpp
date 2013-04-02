8d34f50d04774bab6464acfcc65bca28288ae20e
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2013-04-02 19:25:03 -0300

    Adicionada estrutura para fazer os selects na base
    E adicionado flag de C++11 para compilação dos fontes

---
+++ controller/qmlinterface.cpp
@@ -0,0 +1,64 @@
+#include <QSqlRecord>
+#include <QHash>
+#include <QSqlField>
+#include "qmlinterface.h"
+
+QMLInterface::QMLInterface(QObject *parent)
+	: QAbstractItemModel(parent)
+	, m_columnCount(0)
+	, m_query(NULL)
+{
+}
+
+void QMLInterface::loadFieldMetaData()
+{
+	QHash<int, QByteArray> rolenames;
+	QSqlRecord record = m_query.record();
+
+	m_columnCount = record.count();
+
+	for (int i = 0; i < m_columnCount; i++) {
+		QString name = record.fieldName(i);
+		rolenames.insert(Qt::UserRole + i + 1, name.toUtf8());
+	}
+
+	setRoleNames(rolenames);
+}
+
+QVariant QMLInterface::data(const QModelIndex &index, int role) const
+{
+        int columnIdx = role < Qt::UserRole ? index.column() : role - Qt::UserRole - 1;
+        if ( role == Qt::DisplayRole || role > Qt::UserRole )
+                return m_dataList[index.row()][columnIdx];
+        return QVariant();
+}
+
+int QMLInterface::rowCount(const QModelIndex &parent) const 
+{
+        return parent.isValid() ? 0 : m_dataList.size();
+}
+
+QVector<QVariant> QMLInterface::loadListFromCurrentRecord()
+{
+	QVector<QVariant> result;
+	for (int i = 0; i < m_columnCount; i++)
+		result.append(m_query.value(i));
+	return result;
+}
+
+void QMLInterface::exec(QSqlQuery query)
+{
+	m_query = query;
+
+	if ( !m_query.isActive() )
+		m_query.exec();
+
+	loadFieldMetaData();
+
+	while(m_query.next())
+	{
+		QVector<QVariant> cols = loadListFromCurrentRecord();
+		m_dataList.push_back(cols);
+	}
+	reset();
+}
---
+++ controller/qmlinterface.h
@@ -0,0 +1,28 @@
+#ifndef __QMLINTERFACE_H__
+#define __QMLINTERFACE_H__
+
+#include <QAbstractItemModel>
+#include <QSqlQuery>
+#include <QVector>
+#include <QObject>
+#include <QStringList>
+
+class QMLInterface : public QAbstractItemModel
+{
+	Q_OBJECT
+public:
+	QMLInterface(QObject *parent = 0);
+	Q_INVOKABLE void exec(QSqlQuery query);
+	virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
+	virtual QVariant data(const QModelIndex &index, int role) const;
+
+private:
+	int m_columnCount;
+	QSqlQuery m_query;
+	QVector<QVector<QVariant>> m_dataList;
+
+	QVector<QVariant> loadListFromCurrentRecord();
+	void loadFieldMetaData();
+};
+
+#endif //__QMLINTERFACE_H__
