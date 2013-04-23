be3cb46fd8defb855a6513824163849eb9e6de35
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2013-04-22 23:31:00 -0300

    Add Doxygen documentation of QMLInterface class

--- controller/qmlinterface.h
+++ controller/qmlinterface.h
@@ -7,23 +7,26 @@
 #include <QObject>
 #include <QStringList>
 
+/**
+ * QMLInterface - Bring the data of the database
+ */
 class QMLInterface : public QAbstractListModel
 {
 	Q_OBJECT
 public:
-	QMLInterface(QObject *parent = 0);
-	Q_INVOKABLE void exec(QSqlQuery query);
-	Q_INVOKABLE void refresh();
-	virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
-	virtual QVariant data(const QModelIndex &index, int role) const;
+	QMLInterface(QObject *parent = 0); ///< default contructor
+	Q_INVOKABLE void exec(QSqlQuery query); ///< executes a SQL
+	Q_INVOKABLE void refresh(); ///< refresh data bringing the data of database
+	virtual int rowCount(const QModelIndex &parent = QModelIndex()) const; ///< number of rows fetched
+	virtual QVariant data(const QModelIndex &index, int role) const; ///< type of data
 
 private:
-	int m_columnCount;
-	QSqlQuery m_query;
-	QVector<QVector<QVariant>> m_dataList;
+	int m_columnCount; ///< number of columns of the query
+	QSqlQuery m_query; ///< query to execute
+	QVector<QVector<QVariant>> m_dataList; /// data of the database
 
-	QVector<QVariant> loadListFromCurrentRecord();
-	void loadFieldMetaData();
+	QVector<QVariant> loadListFromCurrentRecord(); ///< bring data of the database to a record
+	void loadFieldMetaData(); ///< load data in the field
 };
 
 #endif //__QMLINTERFACE_H__
