1a7c8412c257a4fdf2b243174faa37bb99691a8f
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2013-04-23 19:28:38 -0300

    Added headmode, needs to implement

--- main.cpp
+++ main.cpp
@@ -4,12 +4,23 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-    QDeclarativeView view;
+	QApplication app(argc, argv);
 
-    Engine::start();
+	QStringList args = app.arguments();
 
-    view.setSource(QUrl("qml/Main.qml"));
-    view.show();
-    return app.exec();
+	if (args.count() > 1) {
+		if (args[1] != "--headless") {
+			qDebug() << "Expected --headless, received " << args[1] << ". Aborting!";
+			return 1;
+		}
+		// implement headless here
+	}
+
+	QDeclarativeView view;
+
+	Engine::start();
+
+	view.setSource(QUrl("qml/Main.qml"));
+	view.show();
+	return app.exec();
 }
