1fc1283b9c235a975a430fd8d6fb948d44ca7dc6
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2013-06-17 23:23:51 -0300

    Fixed some comparisons
    Also some code style cleanup

--- main.cpp
+++ main.cpp
@@ -17,26 +17,29 @@ void PrintUsage()
 int ExtractArgs(QStringList * args, QHostAddress * ip, int * port)
 {
 	bool ok = false;
-	if(args->size() >= 4){
-		if(args->at(1) == "-client" | args->at(1)== "-server"){ // first arg ok
-			if(args->at(1) == "-client" & args->size() >= 6){ // validate client args
-				if(args->at(2) == "-ip" & args->at(4) == "-port"){
-					if(args->at(3) != NULL & args->at(5) != NULL){
+	if (args->size() >= 4) {
+
+		if (args->at(1) == "-client" || args->at(1)== "-server" ) { // first arg ok
+
+			if (args->at(1) == "-client" && args->size() >= 6) { // validate client args
+				if (args->at(2) == "-ip" && args->at(4) == "-port") {
+					if (args->at(3) != NULL && args->at(5) != NULL) {
 						ip->setAddress(args->at(3));
 						*port = args->at(5).toInt();
 						ok = true;
 					}
 				}
-			}else if(args->at(1) == "-server"){ // validate server args
-				if(args->at(2) == "-port" & args->at(3) != NULL){
+
+			} else if (args->at(1) == "-server") { // validate server args
+				if (args->at(2) == "-port" && args->at(3) != NULL) {
 					*port = args->at(3).toInt();
 					ok = true;
 				}
-			}	
+			}
 		}
 	}
 
-	if(!ok){
+	if (!ok) {
 		PrintUsage();
 		return -1;
 	}
